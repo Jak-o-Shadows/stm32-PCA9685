9918671733658932bf1061e2e917bd5ec390c140
Jak_o_Shadows <jak-o-shadows@users.noreply.github.com>
2019-12-18 21:01:09 +1030

    Update to Python 3 proper.

--- hardware/code/serialTest.py
+++ hardware/code/serialTest.py
@@ -1,16 +1,25 @@
 import serial
 import time
+import binascii
 
-port = "COM5"
+
+def send(con, num):
+    uint8_t = hex(num)[2:].zfill(2)
+    #input("continue?")
+    b1 = binascii.unhexlify(uint8_t)
+    print("\t", b1)
+    con.write(b1)
+
+port = "COM8"
 baud = 9600
 
 ser = serial.Serial()
 ser.port = port
 ser.baudrate = baud
 
-data1 = [[0xFF, i, 0x00] for i in xrange(2)]
-data2 = [[0xFF, i, 0xFF] for i in xrange(2)]
-data3 = [[0xFF, i+32, 0x7F] for i in xrange(2)]
+data1 = [[0xFF, i, 0x00] for i in range(2)]
+data2 = [[0xFF, i, 0xFF] for i in range(2)]
+data3 = [[0xFF, i+32, 0x7F] for i in range(2)]
 
 
 d1 = [item for sublist in data1 for item in sublist]
@@ -18,18 +27,18 @@ d2 = [item for sublist in data2 for item in sublist]
 d3 = [item for sublist in data3 for item in sublist] + [0xFF, 0xFF, 0xFF]
 
 ser.open()
-for c in xrange(10):
-    print d1
+for c in range(4):
+    print(d1)
     for i in d1:
-        ser.write(chr(i))
+        send(ser, i)
 
-    time.sleep(1)
-    print d2
+    time.sleep(3)
+    print(d2)
     for i in d2:
-        ser.write(chr(i))
-    time.sleep(1)
-    print d3
+        send(ser, i)
+    time.sleep(3)
+    print(d3)
     for i in d3:
-        ser.write(chr(i))
-    time.sleep(1)
+        send(ser, i)
+    time.sleep(10)
 
